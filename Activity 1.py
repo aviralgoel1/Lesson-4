b669d5a02329f033235dd7a4d8d8ef5475253c60
aviralgoel1 <aviralgoel1@gmail.com>
2025-07-25 11:18:15 +0100

    Lesson 4 activities

---
+++ Activity 1.py
@@ -0,0 +1,5 @@
+n=int(input("Enter number you want to find sum for: "))
+sum=0
+for i in range (1, n+1):
+    sum = sum+i
+print("\nSum: ",sum)
---
+++ Activity 2.py
@@ -0,0 +1,8 @@
+string = input("Enter string")
+string2 = ""
+
+for i in string:
+    string2=i+string2
+
+print ("\n The original string = ", string)
+print ("The reversed string =", string2)
